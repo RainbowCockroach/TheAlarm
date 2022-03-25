023a55f899525d850d03b0e4918e68a61e064111
kien <kiennnhe140336@fpt.edu.vn>
2022-03-25 10:04:27 +0700

    alarm front

---
+++ app/src/main/java/com/example/thealarm/alarm.java
@@ -0,0 +1,23 @@
+package com.example.thealarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
+public class alarm extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
